3dada4f1fd4e5bfb587536f041d25fc5bb90e773
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2023-12-13 16:54:39 +0100

    add ciclo for per creare email da axios

--- script.js
+++ script.js
@@ -6,3 +6,30 @@ generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
 Bonus
 Far comparire gli indirizzi email solamente quando sono stati tutti generati.
 */
+
+const { createApp } = Vue
+
+createApp ({
+    data () {
+        return {
+            emails: [],
+        }
+    },
+    methods: {
+
+    },
+    mounted () {
+        console.log("app caricata");
+
+        for (let i = 0; i < 10; i++) {
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then((response) => {
+                this.email = response.data.response;
+                this.emails.push(this.email)
+            })
+        }
+
+
+    }
+
+}).mount("#app")
